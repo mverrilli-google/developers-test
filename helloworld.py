69a262112c55e7e5196b90a23d8156286afb5401
Michael Verrilli <mverrilli@google.com>
2014-01-07 16:29:51 -0500

    Adds first file to project, helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+"""A simple python script.
+
+A python script that prints out "Hello, world!".
+"""
+
+import sys
+
+HELLO_TEXT = "Hello, world!"
+
+
+def main(argv):
+  print HELLO_TEXT
+
+
+if __name__ == '__main__':
+  main(sys.argv)
